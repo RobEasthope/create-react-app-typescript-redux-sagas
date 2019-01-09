4185918d6a006072d47a4dc4c7e01ff141823fa7
Rob Easthope <hello@robeasthope.com>
2019-01-09 11:46:17 +0000

    Remove comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,3 @@
-// @ts-check
-
-// Why does this file use the `.js` extension instead of `.tsx`? It's because Parcel only accepts an
-// `index.js` file as an entry point. However, we can enable TS typechecking by adding the
-// `@ts-check` comment at the beginning of our file.
-
 import { ConnectedRouter } from "connected-react-router";
 import { createHashHistory } from "history";
 import * as React from "react";
