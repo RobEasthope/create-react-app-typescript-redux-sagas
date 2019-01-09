bfa6adb3ec129efcb8b6161491c61688ad85c924
Rob Easthope <hello@robeasthope.com>
2019-01-09 16:59:14 +0000

    Fix font issue

--- src/components/layout/Root.tsx
+++ src/components/layout/Root.tsx
@@ -20,5 +20,6 @@ const Wrapper = styled("div")`
   min-height: 100vh;
   background-color: #ebebea;
   color: #2e2e2c;
-  font-family: "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, Arial, sans-serif";
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, Arial, sans-serif;
 `;
