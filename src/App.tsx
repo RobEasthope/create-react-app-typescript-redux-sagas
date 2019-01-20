f2c30c310c768d0699a82f98b6a6ae7dd213f012
Rob Easthope <hello@robeasthope.com>
2019-01-20 14:00:16 +0000

    Simplify App & index code

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,38 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { Normalize } from 'styled-normalize';
+import { ConnectedRouter } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import React from "react";
+import { Provider } from "react-redux";
+import { Normalize } from "styled-normalize";
 
-import Routes from './routes';
-import { ApplicationState } from './store';
-import { ThemeColors } from './store/layout';
+import Header from "./components/layout/Header";
+import configureStore from "./configureStore";
+import GlobalStyles from "./styles/GlobalStyles";
 
-import GlobalStyles from './styles/GlobalStyles';
+import Routes from "./routes";
 
-// Separate props from state and props from dispatch to their own interfaces.
-interface PropsFromState {
-  theme: ThemeColors;
-}
+const history = createBrowserHistory();
 
-interface PropsFromDispatch {
-  [key: string]: any;
-}
+const initialState = window.initialReduxState;
+const store = configureStore(history, initialState);
 
-// Any additional component props go here.
-interface OwnProps {}
+const App = () => {
+  return (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <React.Fragment>
+          {/* Add global styles */}
+          <Normalize />
+          <GlobalStyles />
 
-// Create an intersection type of the component props and our Redux props.
-type AllProps = PropsFromState & PropsFromDispatch & OwnProps;
+          {/* Add header */}
+          <Header />
 
-class App extends React.Component<AllProps> {
-  public render() {
-    const { theme } = this.props;
+          {/* Main routing */}
+          <Routes />
+        </React.Fragment>
+      </ConnectedRouter>
+    </Provider>
+  );
+};
 
-    return (
-      <div>
-        <Normalize />
-        <GlobalStyles />
-        <Routes />
-      </div>
-    );
-  }
-}
-
-// It's usually good practice to only include one context at a time in a connected component.
-// Although if necessary, you can always include multiple contexts. Just make sure to
-// separate them from each other to prevent prop conflicts.
-const mapStateToProps = ({ layout }: ApplicationState) => ({
-  theme: layout.theme,
-});
-
-// Normally you wouldn't need any generics here (since types infer from the passed functions).
-// But since we pass some props from the `index.js` file, we have to include them.
-// For an example of a `connect` function without generics, see `./containers/LayoutContainer`.
-export default connect<
-  PropsFromState,
-  PropsFromDispatch,
-  OwnProps,
-  ApplicationState
->(mapStateToProps)(App);
+export default App;
