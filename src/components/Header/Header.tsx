475f5ea660e3d62aec695ab8ed7582027bc6e49b
Rob Easthope <hello@robeasthope.com>
2019-01-20 17:22:50 +0000

    Remove title component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { Box, Flex, Heading } from "rebass";
 
-import Title from "../Title/Title";
-
 const Header = () => (
   <Flex px={4} py={4} alignItems="center">
     <Heading fontSize={[4, 5]} color="blue">
--- src/components/Title/Title.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Title = styled.h2`
-  margin: 0;
-  font-weight: 500;
-`;
-
-export default Title;
